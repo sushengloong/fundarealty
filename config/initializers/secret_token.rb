5332d4234072ee635c3457c069b2fb97961b440f
Sheng-Loong Su <sushengloong@gmail.com>
2013-03-09 22:19:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundRealty::Application.config.secret_token = '250353182f87e6f3d12ca3e3466d84501f67e497cd6933bbcc27135caee1fde9062af40f7981294eea3842567a0ef3230f3bea55f9bc804b4aaa175efe794731'
