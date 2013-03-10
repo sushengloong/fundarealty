5b46a58aec20005f6ed705fc7706f4fa70589612
Sheng-Loong Su <sushengloong@gmail.com>
2013-03-10 09:01:36 +0800

    add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def app_display_name
+    "Fund Realty"
+  end
+
+  def app_display_tagline
+    "Asia's First Real Estate Investment Platform"
+  end
+
+  def app_display_title
+    "#{app_display_name} - #{app_display_tagline}"
+  end
+
+  def title page_title
+    content_for(:title) { page_title }
+  end
 end
