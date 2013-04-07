77dfac2922befda57b297794ea7fc86a1a1a7e1d
Sheng-Loong Su <sushengloong@gmail.com>
2013-04-07 23:45:20 +0800

    add additional fields to prelaunch subscription form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,10 +8,16 @@ class PagesController < ApplicationController
   end
 
   def invite
-    Gibbon.list_subscribe id: "13fe50d34b", email_address: params[:email]
+    raise "All fields are mandatory" if [:inputEmail, :inputFirstName, :inputLastName, :inputInvestRange].any? { |field| params[field].blank? }
+    invest_range = params[:inputInvestRange].split('-').map { |amt| amt.to_f }
+
+    Gibbon.list_subscribe id: "13fe50d34b", email_address: params[:inputEmail], merge_vars: { 
+      fname: params[:inputFirstName], lname: params[:inputLastName], min_invest: invest_range[0], max_invest: invest_range[1] }
     @ret = { status: "success", msg: "You're almost done! Please check your mailbox for the confirmation email." }
   rescue Gibbon::MailChimpError => mce
     @ret = { status: "error", msg: mce }
+  rescue Exception => e
+    @ret = { status: "error", msg: e }
   ensure
     render :prelaunch, layout: false
   end
